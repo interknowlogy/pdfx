46e98d89cb509771e05dec7b43333422c1babd7c
KevinStumpf <live@kevinstumpf.de>
2013-05-16 11:49:36 -0700

    Extended BindableBase and BindableExt in a way that they can easily be merged into existing projects that already use 3rd Party Frameworks (such as MVVMLight) - Part II

---
+++ PropertyDependencyFramework/PropertyDependencyFramework/Interfaces/IBindableAccessToProtectedFunctionality.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace PropertyDependencyFramework.Interfaces
+{
+	public interface IBindableAccessToProtectedFunctionality : IBindableBaseAccessToProtectedFunctionality
+	{
+		IDependentProperty TunnelledProperty<T>(Expression<Func<T>> property);
+	}
+}
---
+++ PropertyDependencyFramework/PropertyDependencyFramework/Interfaces/IBindableBaseAccessToProtectedFunctionality.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace PropertyDependencyFramework.Interfaces
+{
+	public interface IBindableBaseAccessToProtectedFunctionality
+	{
+		Delegate[] TunnelledGetPropertyChangedInTransactionInvocationList();
+
+		Delegate[] TunnelledGetPropertyChangedInvocationList();
+
+		void TunnelledNotifyPropertyChanged(string propertyName);
+
+		T TunnelledCachedValue<T>(Expression<Func<T>> ofProperty, Func<T> propertyEvaluation);
+
+		void TunnelledRegisterPropertyDependency<T, T1, T2>(T masterPropertyOwner, Expression<Func<T, T1>> masterProperty,
+		                                           Expression<Func<T2>> dependentProperty) where T : INotifyPropertyChanged;
+
+		void TunnelledRegisterPropertyDependency<T, T1, T2>(DependencyFrameworkObservableCollection<T> masterPropertyOwnerCollection,
+		                                           Expression<Func<T, T1>> masterProperty,
+		                                           Expression<Func<T2>> dependentProperty) where T : INotifyPropertyChanged;
+
+
+		void TunnelledRegisterCallbackDependency<T>(T masterPropertyOwner, Action callback)
+			where T : INotifyPropertyChanged;
+
+		void TunnelledRegisterCallbackDependency<T, T1>(T masterPropertyOwner, Expression<Func<T, T1>> masterProperty, Action callback)
+			where T: INotifyPropertyChanged;
+
+		void TunnelledRegisterCallbackDependency<T, T1>(T[] masterPropertyOwners, Expression<Func<T, T1>> masterProperty,
+		                                       Action callback)
+			where T : INotifyPropertyChanged;
+
+		void TunnelledRegisterCallbackDependency<T>(DependencyFrameworkObservableCollection<T> masterPropertyOwnerCollection,
+		                                   Action callback);
+
+		void TunnelledRegisterCallbackDependency<T, T1>(DependencyFrameworkObservableCollection<T> masterPropertyOwnerCollection,
+		                                       Expression<Func<T, T1>> masterProperty, Action callback)
+			where T : INotifyPropertyChanged;
+
+		void TunnelledUnregisterAllPropertyDependencies();
+
+		void TunnelledUnRegisterPropertyDependency(INotifyCollectionChanged masterPropertyOwnerCollection, string masterPropertyName,
+		                                  string dependantPropertyName);
+
+		void TunnelledUnRegisterPropertyDependency(INotifyPropertyChanged masterPropertyOwner, string masterPropertyName,
+		                                  string dependantPropertyName);
+
+	}
+}
