718b248cfff6810398c4c0b398b22b97cbd2dbc3
dannydwarren <dannydwarren@gmail.com>
2013-12-04 15:23:04 -0800

    Type Registrations plumbing now working! Public API needs some love.

---
+++ PropertyDependencyFramework/PropertyDependencyFramework/DeclarativeAPI/IPropertyDependencyTypeRegistration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+
+namespace PropertyDependencyFramework
+{
+    public interface IPropertyDependencyTypeRegistration
+    {
+        //TODO: New First Arg = "this" with matching type arg
+        IPropertyDependencyTypeRegistration On<TSourceOwner, TSource, TProperty>(Expression<Func<TSourceOwner, TSource>> source, Expression<Func<TProperty>> sourceProperty)
+            where TSource : INotifyPropertyChanged;
+    }
+}
---
+++ PropertyDependencyFramework/PropertyDependencyFramework/Interfaces/ITypeRegistrationAPI.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq.Expressions;
+
+namespace PropertyDependencyFramework.Interfaces
+{
+    public interface ITypeRegistrationAPI
+    {
+        Dictionary<Type, TypeDependencies> DependenciesByType { get; }
+        void RegisterPropertyDependencyForType<TSourceOwner, TSource, TSourceProp>(Expression<Func<TSourceOwner, TSource>> sourceExpression, Expression<Func<TSourceProp>> sourcePropertyExpression, string dependentPropertyName, Type dependentType)
+            where TSource : INotifyPropertyChanged;
+    }
+}
